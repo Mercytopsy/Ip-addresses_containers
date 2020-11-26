ada1da9785cd533f8e8d8db66859856df7ec342f
Babatope Odunayo <Mercytopsy@users.noreply.github.com>
2020-11-26 12:52:33 +0100

    Update new.php

--- new.php
+++ new.php
@@ -3,10 +3,20 @@
    error_reporting(E_ALL);
    $val = $_POST["msg"];
    echo $val;
-   $con = mysqli_connect("localhost", "odunayo", "1234");
-   $db = mysqli_select_db($con,"Ipcontainer");
+   $con = mysqli_connect("aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "y3cratgrhdyh0er4", "wbnk5i1ymw3vukpz");
+   $db = mysqli_select_db($con,"m1jzacxsz4z2bsxn");
   // $information = mysqli_real_escape_string($con, $val);
-   $sql = "INSERT INTO container (information) VALUES ('$val')";
+   $creating = "CREATE TABLE containers(
+	   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY_KEY,
+	   information VARCHAR(50) NOT NULL,
+   )";
+  // $result = mysqli_query($con, $sql);
+   if (mysqli_query($con, $creating)) {
+   echo "Table created successfully";
+   } else {
+   echo "Error creating table". mysqli_error($con);
+   }
+   $sql = "INSERT INTO containers (information) VALUES ('$val')";
   // $result = mysqli_query($con, $sql);
    if (mysqli_query($con, $sql)) {
    echo "New record created successfully";
